ffaf7f5cd7d7ec49db0fe044e0f044fb9ce2bfbb
JunYangZhu <s10248393@connect.np.edu.sg>
2023-02-01 23:07:24 +0800

    first browser tab icon test + update on pages

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-function allowDrop(ev) {
-    ev.preventDefault();
-  }
-  
-  function drag(ev) {
-    ev.dataTransfer.setData("Text", ev.target.id);
-  }
-  
-  function drop(ev) {
-    var data = ev.dataTransfer.getData("Text");
-    ev.target.appendChild(document.getElementById(data));
-    ev.preventDefault();
-  }
+$(document).ready()
